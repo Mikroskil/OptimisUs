df95814aafdf5ef96e293935b53d312931386f50
Hendro Pramana Sinaga <putrajayakesuma@hotmail.com>
2013-11-13 06:10:53 +0700

    Basic
    Basic Design (Interface incomplete)

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+?>
+<!doctype html>
+<html lang="ID">
+<head>
+	<meta charset="utf-8">
+	<title>Selamat Datang di Aplikasi Pencari KORUPSI Medan</title>
+	<meta name="description" content="Aplikasi Pencari KORUPSI (Kost Rumah Kontrakan Sekitar) Medan"/>
+	<meta name="author" content="Aplikasi KORUPSI"/>
+	<link rel="stylesheet" type="text/css" href="csskonf/unvrstyle.css">
+	<style type="text/css">
+	body{width: 100%; height: 100%; margin: 0; padding: 0;} .clearFloating{clear: both;} .encaps-main-contents{background-color: rgb(255,255,255); display: block; height: auto; min-height: 100px; margin: 0 auto; padding: 15px 10px; width: 92%; border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; box-shadow: 0 0 3px silver;}
+	/*adding style to left-main-contents*/
+	#left-main-contents{width: 48%; height: auto; padding: 10px; position: relative; display: inline-block;}
+	/*adding style to right-main-contents*/
+	#right-main-contents{width: 48%; height: auto; padding: 10px; position: relative; display: inline-block;}
+	/*adding style to footer*/
+	.footer-divider{background-color: rgba(42%, 42%, 42%, 0.9271); width: 100%; height: auto; padding: 10px 0; margin: 0; margin-top: 57px; position: relative;}
+	.footer-information{display: block; height: auto; min-height: 100px; margin: 0 auto; padding: 15px 10px; width: 92%; border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px;}
+	</style>
+	<!--[if IE]>
+       	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header class="page-encaps">
+		<div id="kapsul-navigasi">
+			<nav>
+				<ul><li><a href="index.php">Beranda</a></li><li><a href="pages/rilis.php">Terbaru</a></li><li><a href="guide.php">Panduan</a></li><li><a href="tos.php">Aturan Berlaku</a></li></ul>
+			</nav>
+		</div>
+		<div id="kapsul-judul"><h1>Aplikasi Pencari KORUPSI Medan</h1></div>
+	</header><br class="clearFloating">
+	<div class="encaps-main-contents">
+		<div id="left-main-contents">
+			<section id="description-app">
+				<header>
+					<hgroup>
+						<h2>APLIKASI<br>PENCARI KORUPSI</h2><br><h3>(KOST RUMAH PENGINAPAN SEKITAR)</h3><br><h2>MEDAN</h2>
+					</hgroup>
+				</header>
+			</section>
+		</div>
+		<div id="right-main-contents">
+		</div>
+	</div><br class="clearFloating">
+	<div class="footer-divider">
+	<footer class="footer-information">
+		<section id="developer">
+			<article>Pengembang Aplikasi :<br>
+			<ul><li>Hendro Pramana Sinaga</li><li>Putra Jaya Kesuma</li></ul>
+		</section>
+		<section id="contact-us">
+		</section>
+		<section id="copyright">
+		</section>
+	</footer>
+	</div>
+</body>
+</html>
